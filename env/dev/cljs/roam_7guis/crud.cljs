ad04cc2a8c1ae113ec5d25a7b13705ccef12a186
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-11-30 16:26:30 +1000

    CRUD app is working, if a little ugly

--- env/dev/cljs/roam_7guis/crud.cljs
+++ env/dev/cljs/roam_7guis/crud.cljs
@@ -19,6 +19,7 @@
   {:names {"0" ["Ben" "Follington"]
            "1" ["Conor" "White-Sullivan"]
            "2" ["Michael" "Ashcroft"]}
+   :last-generated-id 2
    :filter ""
    :edit-form {:first ""
                :last ""}
@@ -49,14 +50,16 @@
      (map (fn [[i v]] [:option {:key i
                                 :value i} (format-name v)]) names))])
 
-(defn raw-input [state path]
+(defn raw-input
+  "an input field that directly updates the atom storing its value"
+  [state path]
   [:input {:type "text"
            :value (get-in @state path)
            :on-change (fn [e]
                         (let [value (-> e .-target .-value)]
                           (swap! state #(assoc-in % path value))))}])
 
-(defn edit-form [state selected-index]
+(defn edit-form [state]
   [v-box
    :children [[raw-input state [:edit-form :first]]
               [raw-input state [:edit-form :last]]]])
@@ -66,12 +69,29 @@
            :value (:filter @state)
            :on-change (fn [e] (let [value (-> e .-target .-value)]
                                 (set-state! state :filter value)))}])
+(defn generate-id! [state]
+  (swap! state #(update % :last-generated-id inc))
+  (str (:last-generated-id @state)))
+
+(defn add-entry! [state]
+  (let [form (:edit-form @state)
+        entry [(:first form) (:last form)]]
+    (swap! state #(assoc-in % [:names (generate-id! state)] entry))))
+
+(defn update-entry! [state]
+  (let [form (:edit-form @state)
+        entry [(:first form) (:last form)]]
+    (swap! state #(assoc-in % [:names (:selected-id @state)] entry))))
+
+(defn delete-entry! [state]
+  (swap! state #(update-in % [:names] dissoc (:selected-id @state)))
+  (set-state! state :edit-form {:first "" :last ""}))
 
 (defn action-buttons [state]
   [h-box
-   :children [[:button "Create"]
-              [:button "Update"]
-              [:button "Delete"]]])
+   :children [[:button {:on-click #(add-entry! state)} "Create"]
+              [:button {:on-click #(update-entry! state)} "Update"]
+              [:button {:on-click #(delete-entry! state)} "Delete"]]])
 
 (defn crud []
   (let [state (atom initial-state)]
@@ -81,5 +101,5 @@
        :children [[filter-field state]
                   [h-box
                    :children [[name-list state]
-                              [edit-form state (:selected-id @state)]]]
+                              [edit-form state]]]
                   [action-buttons state]]])))
