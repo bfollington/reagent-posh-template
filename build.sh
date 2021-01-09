82abfda096c61334255b72560e863575784c0739
Ben Follington <5009316+bfollington@users.noreply.github.com>
2021-01-09 14:45:19 +1000

    One command to release on itch

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "building..."
+lein uberjar
+
--- release.sh
+++ release.sh
@@ -1,6 +1,3 @@
-echo "building..."
-lein uberjar
-
 echo "preparing..."
 mkdir -p ./dist/js
 mkdir -p ./dist/css
@@ -8,11 +5,14 @@ mkdir -p ./dist/assets
 
 echo "copying files..."
 cp ./target/cljsbuild/public/js/app.js ./dist/js/app.js
-cp -r ./resources/public/css ./dist/css
-cp -r ./resources/public/assets ./dist/assets
+cp -r ./resources/public/css ./dist
+cp -r ./resources/public/assets ./dist
 cp ./resources/public/index.html ./dist
 
 echo "packaging..."
-zip dist.zip dist/**/*
+zip -r dist.zip ./dist
+
+echo "uploading..."
+butler push dist.zip twopm/in-passing:web
 
 echo "done!"
